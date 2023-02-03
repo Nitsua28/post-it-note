93d17b76a0456d9c0eaa570c09ab7fc9adabdba8
Nitsua28 <achan113@ucmerced.edu>
2023-02-02 16:12:00 -0800

    functionality works

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+import { PostState, postItReducer } from './postItReducer';
 
 function App() {
+  const [PostState, dispatch] = useReducer(postItReducer,{input: "",postItArr:[]})
+  const listItems = PostState.postItArr.map((i) => <li>{i}</li>)
   return (
-    <h1>ssd</h1>
+    <>
+      <input onChange={(event)=>dispatch({type:"INPUT", payload: event.target.value})}></input>
+      <button onClick={()=>dispatch({type:"POST"})}>Post</button>
+      <button onClick={()=>dispatch({type:"CLEAR"})}>Clear</button>
+      <button onClick={()=>dispatch({type:"DELETE"})}>Delete</button>
+      <ul>{listItems}</ul>
+    </>
   );
 }
 
---
+++ src/postItReducer.ts
@@ -0,0 +1,36 @@
+
+export type PostState = {
+    input: string,
+    postItArr: string[]
+}
+
+export type InputAction = {type: "INPUT", payload: string};
+export type PostAction = {type: "POST"};
+export type ClearAction = {type: "CLEAR"};
+export type DeleteAction = {type: "DELETE"};
+export type PostItAction = ClearAction | PostAction | InputAction | DeleteAction;
+
+export function postItReducer(state: PostState, action: PostItAction): PostState{
+    const newState: PostState = JSON.parse(JSON.stringify(state));// make deep copy
+
+    switch(action.type){
+        case "INPUT":{
+            newState.input = action.payload;
+            return newState;
+        }
+        case "POST":{
+            newState.postItArr.push(newState.input);
+            return newState;
+        }
+        case "CLEAR":{
+            newState.postItArr = [];
+            return newState;
+        }
+        case "DELETE":{
+            let newArr = newState.postItArr;
+            newArr.pop()
+            newState.postItArr = newArr;
+            return newState;
+        }
+    }
+}
